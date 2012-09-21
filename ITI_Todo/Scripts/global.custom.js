0bcdf809ca9df32cc72906b31e7507829a55a2ec
Justin Obney <Justin Obney>
2012-09-21 16:55:26 -0500

    More javascript foo.. ajax form submissions

---
+++ ITI_Todo/Scripts/global.custom.js
@@ -0,0 +1,51 @@
+﻿// =================================
+//     Custom jQuery Plugins...
+// =================================
+
+(function ($) {
+    var o = $({});
+    $.subscribe = function () {
+        o.on.apply(o, arguments);
+    };
+    $.unsubscribe = function () {
+        o.off.apply(o, arguments);
+    };
+    $.publish = function () {
+        o.trigger.apply(o, arguments);
+    };
+
+    $.fn.serializeObject = function () {
+        var o = {};
+        var a = this.serializeArray();
+        $.each(a, function () {
+            if (o[this.name] !== undefined) {
+                if (!o[this.name].push) {
+                    o[this.name] = [o[this.name]];
+                }
+                o[this.name].push(this.value || '');
+            } else {
+                o[this.name] = this.value || '';
+            }
+        });
+        return o;
+    };
+})(jQuery);
+
+window.log = function f() {
+    log.history = log.history || [];
+    log.history.push(arguments);
+    if (this.console) {
+        var args = arguments,
+                        newarr;
+        args.callee = args.callee.caller;
+        newarr = [].slice.call(args);
+        if (typeof console.log === 'object') log.apply.call(console.log, console, newarr);
+        else console.log.apply(console, newarr);
+    }
+};
+(function (a) {
+    function b() { }
+    for (var c = "assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","), d; !!(d = c.pop()); ) {
+        a[d] = a[d] || b;
+    }
+})(function () { try { console.log(); return window.console; } catch (a) { return (window.console = {}); } } ());
