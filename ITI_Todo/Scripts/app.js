a267138982c55c79d53577d9b9b5ada4e03765b2
Justin Obney <Justin Obney>
2012-09-25 16:54:28 -0500

    Moved app javascript to the app.js file

--- ITI_Todo/Scripts/app.js
+++ ITI_Todo/Scripts/app.js
@@ -1,6 +1,33 @@
-(function( window ) {
+(function( window, $ ) {
 	'use strict';
 
-	// Your starting point. Enjoy the ride!
+	toastr.options = {
+	    positionClass: 'toast-bottom-right',
+	    timeOut: 10000
+	}
 
-})( window );
+	$("form[action*='Todo/MarkComplete'] :checkbox").on("change", function (event) {
+	    toastr.info("Not Implemented", "Completed Action Captured");
+	});
+
+	$("form[action*='Todo/Delete']").on("submit", function (event) {
+	    event.preventDefault();
+
+	    toastr.error("Not Implemented", "Delete Action Captured");
+	});
+
+	$("#form_Create").on("submit", function (event) {
+	    event.preventDefault();
+	    $(this).ajaxSubmit({
+	        success: function (data) {
+	            toastr.success("Not Implemented", "Create New Action Captured");
+	            //window.log(data);
+	        },
+	        error: function (jqXHR, textStatus, errorThrown) {
+	            toastr.error(errorThrown, "Create New Action Captured");
+	            //window.log(arguments);
+	        }
+	    });
+	});
+
+})( window, jQuery );
