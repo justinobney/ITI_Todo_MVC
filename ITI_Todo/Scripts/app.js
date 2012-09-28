6e600bb11c5c055fa38b1ea9ff53c60fde74e53b
Justin Obney <justinobney@gmail.com>
2012-09-27 21:01:25 -0500

    Refactored the app.'s file to be more event driven accounted for not having to bind individual form submit handlers

--- ITI_Todo/Scripts/app.js
+++ ITI_Todo/Scripts/app.js
@@ -4,59 +4,73 @@
     toastr.options = {
         positionClass: 'toast-bottom-right',
         timeOut: 10000
-    }
+    };
+
+    var Todo_Actions = {
+        Create: function(_form){
+            var self = $(_form);
+            if (!self.find('#new-todo').val()) {
+                toastr.error("Invalid Data", "Create New Todo");
+                return;
+            }
+
+            self.ajaxSubmit({
+                success: function (data) {
+                    toastr.success("Saved to database", "Create New Todo");
+                    $('#new-todo').val('');
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    toastr.error(errorThrown, "Create New Action Captured");
+                },
+                target: '#todo-list'
+            });
+        },
+        MarkComplete: function(_form){
+            $(_form).ajaxSubmit({
+                success: function (data) {
+                    toastr.info("Not Implemented", "Completed Action Captured<br />" + data.response);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    toastr.error(errorThrown, "Completed Action Captured");
+                }
+            });
+        },
+        Delete: function(_form){
+            $(_form).ajaxSubmit({
+                success: function (data) {
+                    toastr.info("Not Implemented", "Delete Action Captured<br />" + data.response);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    toastr.error(errorThrown, "Delete Action Captured");
+                }
+            });
+        }
+    };
 
     $("form[action*='Todo/MarkComplete'] :checkbox").on("change", function (event) {
-        var self = $(this);
-        var checked = self.prop('checked');
-        console.log(checked);
-
-        $("form[action*='Todo/MarkComplete']").ajaxSubmit({
-            data: { complete: checked },
-            success: function (data) {
-                toastr.info("Not Implemented", "Completed Action Captured<br />" + data.response);
-                //window.log(data);
-            },
-            error: function (jqXHR, textStatus, errorThrown) {
-                toastr.error(errorThrown, "Completed Action Captured");
-                //window.log(arguments);
-            }
-        });
+        $(this).closest('form').trigger('submit');
     });
 
-    $("form[action*='Todo/Delete']").on("submit", function (event) {
-        event.preventDefault();
-        $(this).ajaxSubmit({
-            success: function (data) {
-                toastr.info("Not Implemented", "Delete Action Captured<br />" + data.response);
-                //window.log(data);
-            },
-            error: function (jqXHR, textStatus, errorThrown) {
-                toastr.error(errorThrown, "Delete Action Captured");
-                //window.log(arguments);
-            }
-        });
-    });
-
-    $("#form_Create").on("submit", function (event) {
-        event.preventDefault();
-        var self = $(this);
-        if (!self.find('#new-todo').val()) {
-            toastr.error("Invalid Data", "Create New Todo");
-            return;
+    $("body").on("submit", function (event) {
+        switch (true) {
+          case /Todo\/Create/.test(event.target.action):
+            event.preventDefault();
+            console.log("• Matched 'Create' test");
+            //Todo_Actions.Create(event.target);
+            break;
+          case /Todo\/MarkComplete/.test(event.target.action):
+            event.preventDefault();
+            console.log("• Matched 'MarkComplete' test");
+            //Todo_Actions.MarkComplete(event.target);
+            break;
+          case /Todo\/Delete/.test(event.target.action):
+            event.preventDefault();
+            console.log("• Matched 'Delete' test");
+            //Todo_Actions.Delete(event.target);
+            break;
+          default:
+            console.log("• Didn't match any test");
+            break;
         }
-
-        $(this).ajaxSubmit({
-            success: function (data) {
-                toastr.success("Saved to database", "Create New Todo");
-                $('#new-todo').val('');
-            },
-            error: function (jqXHR, textStatus, errorThrown) {
-                toastr.error(errorThrown, "Create New Action Captured");
-                //window.log(arguments);
-            },
-            target: '#todo-list'
-        });
     });
-
 })(window, jQuery);
