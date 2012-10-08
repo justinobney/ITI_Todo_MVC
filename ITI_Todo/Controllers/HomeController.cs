1bea8ff8ab0a50e94374846e7b8caa870b8651c5
Justin Obney <Justin Obney>
2012-10-08 10:16:02 -0500

    Added google analytics

--- ITI_Todo/Controllers/HomeController.cs
+++ ITI_Todo/Controllers/HomeController.cs
@@ -14,19 +14,5 @@ namespace ITI_Todo.Controllers
 
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your app description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
